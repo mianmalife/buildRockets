805d55deaf70e2a0b5b4cf07137fcf5e463d7f65
mianmalife <253590433@qq.com>
2022-08-11 23:39:13 +0800

    feat: 添加哈希表注释

--- js-data-structures-and-algorithms/hashTable.js
+++ js-data-structures-and-algorithms/hashTable.js
@@ -1,9 +1,14 @@
-// 哈希表实现
+// 哈希表实现 - 哈希表是基于数组实现
+// 优点: 插入 删除 效率高 时间复杂都接近O(1) 比树快 编码容易 瞬时查找 数组基于内容查找 只能从头遍历 删除元素需要位移其它元素 修改操作效率低
+// 不足: 数据无序 无法从小到大排序 key不允许重复
+// 解决冲突使用的链地址法(冲突: 下标重复) Java中的HashMap就使用的链地址法
+// 解决冲突还可以使用开放地址法(寻找空白单元格添加重复元素(线性探测/二次探测/再哈希法))
 // 结构 [ [[k,v],[k,v]], [[k,v],[k,v]], [[k,v],[k,v]], [[k,v],[k,v]] ]
+// 装填因子=哈希表数据总个数/哈希表长度 > 0.75 时需要扩容 扩容一般扩容2倍并设置为质数
 function HashTable() {
   // 用来存放数据
   this.storage = []
-  // 当前哈希表的长度
+  // 当前哈希表的元素总个数
   this.count = 0
   // 哈希表的长度 后面扩容用
   this.limit = 7
@@ -40,7 +45,7 @@ function HashTable() {
     // 添加
     bucket.push([key, value])
     this.count += 1
-    // 扩容
+    // 扩容 - 装填因子大于0.75 扩容后的limit最好是质数
     if (this.count > this.limit * 0.75) {
       const newLimit = this.limit * 2
       const newPrime = this.getPrime(newLimit)
@@ -121,7 +126,11 @@ function HashTable() {
     }
   }
 
-  // 判断质数
+  // 判断质数 质数：只能被1和自身整除的数
+  // 一个数如果能因数分解 则分解得到的数 一个一定小于等于sqrt(num) 一个一定大于等于sqrt(num)
+  // sqrt(16) 2*8 4*4
+  // 方法1 遍历 2-num-1 如果都不能整除 则为质数
+  // 方法2 遍历2-sqrt(num) 如果都不能整除 则为质数
   HashTable.prototype.isPrime = function (num) {
     let sqrtNum = parseInt(Math.sqrt(num), 10)
     for (let i = 2; i <= sqrtNum; i++) {
