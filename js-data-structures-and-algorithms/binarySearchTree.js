e56411b221e201114d9ed0d109c07aa2276afed9
mianmalife <253590433@qq.com>
2022-08-20 23:32:46 +0800

    feat: 二叉树删除操作部分代码

--- js-data-structures-and-algorithms/binarySearchTree.js
+++ js-data-structures-and-algorithms/binarySearchTree.js
@@ -131,6 +131,49 @@ function BinarySearchTree() {
     }
     return null
   }
+
+  // 删除操作
+  BinarySearchTree.prototype.remove = function (key) {
+    let current = this.root
+    let parent = null
+    let isLeftChild = true
+    while (current.key !== key) {
+      parent = current
+      if (current.key > key) {
+        isLeftChild = true
+        current = current.left
+      } else {
+        isLeftChild = false
+        current = current.right
+      }
+      if (current === null) return false
+    }
+    if (current.left === null && current.right === null) {
+      if (current === this.root) {
+        this.root = null
+      } else if (isLeftChild) {
+        parent.left = null
+      } else {
+        parent.right = null
+      }
+    } else if (current.left === null) {
+      if (current === this.root) {
+        this.root = current.right
+      } else if (isLeftChild) {
+        parent.left = current.right
+      } else {
+        parent.right = current.right
+      }
+    } else if (current.right === null) {
+      if (current === this.root) {
+        this.root = current.left
+      } else if (isLeftChild) {
+        parent.left = current.left
+      } else {
+        parent.right = current.left
+      }
+    }
+  }
 }
 
 let bstStr = ''
